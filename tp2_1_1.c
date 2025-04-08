6b95ca68ed6e7ebaeb9db1c99b95b9025a9d269e
tomas <tomivega027@gmail.com>
2025-04-07 21:48:56 -0300

    inicial

--- tp2_1_1.c
+++ tp2_1_1.c
@@ -1,20 +1,18 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+// codigo a completar
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define N 20
 
-int main() {
-    int i;
-    double vt[N];
-
-    srand(time(NULL));
-
-    for(i = 0; i < N; i++) {
-        vt[i] = 1 + rand() % 100; 
-        printf("%f ", vt[i]); 
+int main(){
+int i;
+double vt[N];
+    for(i = 0 ; i<N ; i++)
+    {
+        vt[i] = 1 + rand() % 100;
+        printf("%f\n", vt[i]);
     }
-
+    
     return 0;
 }
-
